a55cc01c06386b9d987ce7271010a6433a982d5e
giselberacht <45728669+giselberacht@users.noreply.github.com>
2022-02-15 18:56:16 +0100

    main gameplay done + android build

--- TheoryRepopierdu/Assets/Scripts/GameManager.cs
+++ TheoryRepopierdu/Assets/Scripts/GameManager.cs
@@ -6,23 +6,26 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] public GameObject crossPrefab;
     [SerializeField] public GameObject circlePrefab;
+    [SerializeField] Animation endAnimation;
 
     public static GameManager Instance;
 
-    public bool circleTurn;
-    public bool crossTurn;
+    [HideInInspector] public bool circleTurn;
+    [HideInInspector] public bool crossTurn;
+    [HideInInspector] public bool circleVictory=false;
+    [HideInInspector] public bool crossVictory=false;
     [SerializeField] GameObject circleIndicator;
     [SerializeField] GameObject crossIndicator;
     public BoardSpace[] boardSpace  = new BoardSpace[10];
-    
+    string Victor;
 
 
     private void Awake()
     {
         Instance = this;
-        circleTurn = false;
-        crossTurn = false;
+        NewGame();        
     }
+
     void Start()
     {
         RandomFirstPlayer();
@@ -32,6 +35,7 @@ public class GameManager : MonoBehaviour
     {
         IndicatorActivator();
     }
+
     void RandomFirstPlayer()
     {
         int r = Random.Range(1, 3);
@@ -44,6 +48,7 @@ public class GameManager : MonoBehaviour
             circleTurn = true;
         }
     }
+
     void IndicatorActivator()
     {
         if (circleTurn)
@@ -57,24 +62,105 @@ public class GameManager : MonoBehaviour
             crossIndicator.gameObject.SetActive(true);
         }
     }
+
+    public void GameOverCheck()
+    {
+        LineCheck();
+        ColumnCheck();
+        CurveCheck();
+        VictoryCheck();
+    }
+
     void LineCheck()
     {
+        for (int i = 0; i < 7; i += 3)
+        {
+            if(boardSpace[1+i].isCircle&& boardSpace[2+i].isCircle&& boardSpace[3+i].isCircle)
+            {
+                circleVictory = true;
+                Debug.Log("Circle Line!!!");
+            }
 
+        }
+        for (int i = 0; i < 7; i += 3)
+        {
+            if (boardSpace[1+i].isCross && boardSpace[2+i].isCross && boardSpace[3+i].isCross)
+            {
+                crossVictory = true;
+                Debug.Log("Cross Line!!!");
+            }
+
+        }
     }
+
     void ColumnCheck() 
     {
-        
+        for (int i = 0; i < 4; i += 1)
+        {
+            if (boardSpace[i].isCircle && boardSpace[3 + i].isCircle && boardSpace[6 + i].isCircle)
+            {
+                circleVictory = true;
+                Debug.Log("Circle Column!!!");
+            }
+
+        }
+        for (int i = 0; i < 4; i += 1)
+        {
+            if (boardSpace[i].isCross && boardSpace[3 + i].isCross && boardSpace[6 + i].isCross)
+            {
+                crossVictory = true;
+                Debug.Log("Cross Column!!!");
+            }
+
+        }
     }
+
     void CurveCheck()
     {
-
+        if (boardSpace[1].isCircle && boardSpace[5].isCircle && boardSpace[9].isCircle || boardSpace[3].isCircle && boardSpace[5].isCircle && boardSpace[7].isCircle)
+        {
+            circleVictory = true;
+            Debug.Log("Circle Curve!!!");
+        }
+        if (boardSpace[1].isCross && boardSpace[5].isCross && boardSpace[9].isCross || boardSpace[3].isCross && boardSpace[5].isCross && boardSpace[7].isCross)
+        {
+            crossVictory = true;
+            Debug.Log("Cross Curve!!!");
+        }
     }
+
+    void VictoryCheck()
+    {
+        if (circleVictory)
+        {
+            endAnimation.Play();
+            Victor = "Circle wins!";
+        }
+        if (crossVictory)
+        {
+            endAnimation.Play();
+            Victor = "Cross wins!";
+        }
+    }
+
+    void NewGame()
+    {
+        circleTurn = false;
+        crossTurn = false;
+    }
+
+    void RestartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+
     [System.Serializable]
     public struct BoardSpace
     {
         public int spaceIndex;
         public bool isCircle;
         public bool isCross;
+
         public BoardSpace(int spaceIndex, bool isCircle, bool isCross)
         {
             this.spaceIndex = spaceIndex;
--- TheoryRepopierdu/Assets/Scripts/SymbolSpawner.cs
+++ TheoryRepopierdu/Assets/Scripts/SymbolSpawner.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class SymbolSpawner : MonoBehaviour
+public class SymbolSpawner : MonoBehaviour, IPointerDownHandler
 {
     GameManager instance;
-    
+
     Vector3 position;
     Vector3 positionOffset = new Vector3(0, 0, -90);
     BoxCollider boxCollider;
-    
+
     public int spaceIndex;
     [SerializeField] bool isCross;
     [SerializeField] bool isCircle;
@@ -20,14 +22,18 @@ public class SymbolSpawner : MonoBehaviour
         instance = GameManager.Instance;
         position = transform.position + positionOffset;
         boxCollider = this.gameObject.GetComponent<BoxCollider>();
-        spaceIndex = int.Parse(this.gameObject.name); 
-    }
-    private void OnMouseDown()
-    {
-        SpawnShape();
-        SendSpaceData(spaceIndex,isCircle,isCross);
+        spaceIndex = int.Parse(this.gameObject.name);
     }
 
+    /*
+    private void OnMouseDown()
+    {
+        if (!instance.crossVictory && !instance.circleVictory)
+            SpawnShape();
+        SendSpaceData(spaceIndex, isCircle, isCross);
+        instance.GameOverCheck();
+    }
+    */
     private void SpawnShape()
     {
         if (GameManager.Instance.crossTurn)
@@ -47,19 +53,32 @@ public class SymbolSpawner : MonoBehaviour
             return;
         }
     }
+
     void SpawnCross()
     {
-        Instantiate(instance.crossPrefab, position, instance.crossPrefab.transform.rotation);
+        var spawnedShape = Instantiate(instance.crossPrefab, position, instance.crossPrefab.transform.rotation);
+        spawnedShape.transform.parent = gameObject.transform;
         boxCollider.enabled = false;
     }
+
     void SpawnCircle()
     {
-        Instantiate(instance.circlePrefab, position, instance.circlePrefab.transform.rotation);
+        var spawnedShape = Instantiate(instance.circlePrefab, position, instance.circlePrefab.transform.rotation);
+        spawnedShape.transform.parent = gameObject.transform;
         boxCollider.enabled = false;
     }
-    void SendSpaceData(int x,bool y, bool z)
+
+    void SendSpaceData(int x, bool y, bool z)
     {
         instance.boardSpace[spaceIndex] = new GameManager.BoardSpace(x, y, z);
     }
-    
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!instance.crossVictory && !instance.circleVictory)
+            SpawnShape();
+        SendSpaceData(spaceIndex, isCircle, isCross);
+        instance.GameOverCheck();
+    }
+   
 }
