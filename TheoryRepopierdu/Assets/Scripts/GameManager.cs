4eebf14b7317f558d993495ceca52c537b361eeb
giselberacht <45728669+giselberacht@users.noreply.github.com>
2022-02-15 10:10:42 +0100

    almost all done

--- TheoryRepopierdu/Assets/Scripts/GameManager.cs
+++ TheoryRepopierdu/Assets/Scripts/GameManager.cs
@@ -6,19 +6,80 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] public GameObject crossPrefab;
     [SerializeField] public GameObject circlePrefab;
+
     public static GameManager Instance;
+
+    public bool circleTurn;
+    public bool crossTurn;
+    [SerializeField] GameObject circleIndicator;
+    [SerializeField] GameObject crossIndicator;
+    public BoardSpace[] boardSpace  = new BoardSpace[10];
+    
+
 
     private void Awake()
     {
         Instance = this;
+        circleTurn = false;
+        crossTurn = false;
     }
     void Start()
     {
-        
+        RandomFirstPlayer();
     }
 
     void Update()
     {
+        IndicatorActivator();
+    }
+    void RandomFirstPlayer()
+    {
+        int r = Random.Range(1, 3);
+        if (r == 1)
+        {
+            crossTurn = true;
+        }
+        else
+        {
+            circleTurn = true;
+        }
+    }
+    void IndicatorActivator()
+    {
+        if (circleTurn)
+        {
+            circleIndicator.gameObject.SetActive(true);
+            crossIndicator.gameObject.SetActive(false);
+        }
+        if (crossTurn)
+        {
+            circleIndicator.gameObject.SetActive(false);
+            crossIndicator.gameObject.SetActive(true);
+        }
+    }
+    void LineCheck()
+    {
+
+    }
+    void ColumnCheck() 
+    {
         
+    }
+    void CurveCheck()
+    {
+
+    }
+    [System.Serializable]
+    public struct BoardSpace
+    {
+        public int spaceIndex;
+        public bool isCircle;
+        public bool isCross;
+        public BoardSpace(int spaceIndex, bool isCircle, bool isCross)
+        {
+            this.spaceIndex = spaceIndex;
+            this.isCircle = isCircle;
+            this.isCross = isCross;
+        }
     }
 }
--- TheoryRepopierdu/Assets/Scripts/SymbolSpawner.cs
+++ TheoryRepopierdu/Assets/Scripts/SymbolSpawner.cs
@@ -4,20 +4,62 @@ using UnityEngine;
 
 public class SymbolSpawner : MonoBehaviour
 {
+    GameManager instance;
+    
     Vector3 position;
     Vector3 positionOffset = new Vector3(0, 0, -90);
-    GameManager instance;
+    BoxCollider boxCollider;
+    
+    public int spaceIndex;
+    [SerializeField] bool isCross;
+    [SerializeField] bool isCircle;
 
 
     private void Start()
     {
         instance = GameManager.Instance;
         position = transform.position + positionOffset;
+        boxCollider = this.gameObject.GetComponent<BoxCollider>();
+        spaceIndex = int.Parse(this.gameObject.name); 
     }
     private void OnMouseDown()
     {
-        //add if function to check turn
-        Instantiate(instance.crossPrefab, position, instance.crossPrefab.transform.rotation);
-        Instantiate(instance.circlePrefab, position, instance.circlePrefab.transform.rotation);
+        SpawnShape();
+        SendSpaceData(spaceIndex,isCircle,isCross);
     }
+
+    private void SpawnShape()
+    {
+        if (GameManager.Instance.crossTurn)
+        {
+            SpawnCross();
+            isCross = true;
+            GameManager.Instance.crossTurn = false;
+            GameManager.Instance.circleTurn = true;
+            return;
+        }
+        if (GameManager.Instance.circleTurn)
+        {
+            SpawnCircle();
+            isCircle = true;
+            GameManager.Instance.crossTurn = true;
+            GameManager.Instance.circleTurn = false;
+            return;
+        }
+    }
+    void SpawnCross()
+    {
+        Instantiate(instance.crossPrefab, position, instance.crossPrefab.transform.rotation);
+        boxCollider.enabled = false;
+    }
+    void SpawnCircle()
+    {
+        Instantiate(instance.circlePrefab, position, instance.circlePrefab.transform.rotation);
+        boxCollider.enabled = false;
+    }
+    void SendSpaceData(int x,bool y, bool z)
+    {
+        instance.boardSpace[spaceIndex] = new GameManager.BoardSpace(x, y, z);
+    }
+    
 }
